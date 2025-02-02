dedf4b71faaafed733975d65db8c4cf0100bc4a0
Dreckiez <143894214+Dreckiez@users.noreply.github.com>
2025-02-02 20:40:12 +0700

    About Page

--- script/render_contact.js
+++ script/render_contact.js
@@ -1,8 +1,8 @@
 const CONTACT_INFO = [
-  { title: "GitHub", href: "https://github.com/Dreckiez", class : "fa-brands fa-github", color: "#FFF"},
-  { title: "LinkedIn", href : "https://www.linkedin.com/in/xu%C3%A2n-h%C6%B0ng-mai-44960a334/", class : "fa-brands fa-linkedin", color: "#FFF"},
-  { title: "Discord", href: "https://discord.com/users/342532677859278848", class : "fa-brands fa-discord", color: "#FFF" },
-  { title: "Facebook", href: "https://www.facebook.com/profile.php?id=100008938305451", class : "fa-brands fa-facebook", color : "#FFF" }
+  { title: "GitHub", href: "https://github.com/Dreckiez", class : "fa-brands fa-github", color: "#eef0f4"},
+  { title: "LinkedIn", href : "https://www.linkedin.com/in/xu%C3%A2n-h%C6%B0ng-mai-44960a334/", class : "fa-brands fa-linkedin", color: "#eef0f4"},
+  { title: "Discord", href: "https://discord.com/users/342532677859278848", class : "fa-brands fa-discord", color: "#eef0f4" },
+  { title: "Facebook", href: "https://www.facebook.com/profile.php?id=100008938305451", class : "fa-brands fa-facebook", color : "#eef0f4" }
 ];
 
 const contactContainer = document.querySelector(".PF ul");
