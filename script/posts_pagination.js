df982d2197fe6c89286a4ba772415b36d34e832f
Dreckiez <dreckiez@vnpt.homeeee>
2025-02-04 20:30:56 +0700

    Posts Page

---
+++ script/posts_pagination.js
@@ -0,0 +1,95 @@
+const blogPage = document.getElementById("blogs-page");
+
+fetch("../posts/blogs.json")
+    .then(response => response.json())
+    .then(blogs => {
+        const sortPost = blogs.sort(function(a,b){
+            const DateA = ParseDate(a.date);
+            const DateB = ParseDate(b.date);
+            return DateB - DateA;
+        });
+
+        const totalPages = Math.ceil(sortPost.length/4);
+        let currentPage = 1;
+
+        displayPosts(currentPage, sortPost, totalPages);
+        // PaginationNav(sortPost, totalPages)
+    })
+    .catch(error => console.error("Can't Fetch Blogs:", error));
+
+function displayPosts(page, posts, totalPages) {
+    const startIndex = (page - 1) * 4;
+    const endIndex = startIndex + 4;
+    const paginatedPosts = posts.slice(startIndex, endIndex);
+    blogPage.innerHTML = ''; // Clear existing posts
+    const promises = paginatedPosts.map(post =>
+        fetch(`../posts/${post.file}`).then(res => res.json())
+    );
+    Promise.all(promises)
+        .then(latestPosts => {
+            latestPosts.forEach(post => {
+                const card = createPost(post);
+                blogPage.appendChild(card);
+            });
+            PaginationNav(page, posts, totalPages);
+        })
+        .catch(error => console.error("Can't Fetch Blog Posts:", error));
+}
+
+function PaginationNav(current, posts, totalPages){
+    let PagiNav = document.createElement('div');
+    PagiNav.id = "pagi-nav";
+    for (let i = 1; i <= totalPages; i++) {
+        const pageButton = document.createElement('button');
+        if (i == current) pageButton.classList.add("active");
+        else pageButton.classList.add("inactive")
+        pageButton.innerText = i;
+        pageButton.addEventListener('click', () => {
+            currentPage = i;
+            displayPosts(currentPage, posts, totalPages);
+        });
+        PagiNav.appendChild(pageButton);
+    }
+    blogPage.appendChild(PagiNav);
+}
+
+function ParseDate(date){
+    const [day, month, year] = date.split("-");
+    return new Date(`${year}-${month}-${day}`);
+}
+
+function createPost(post){
+    const card = document.createElement("div");
+    card.className = "card";
+    const img_path = `../posts/${encodeURIComponent(post.title)}/${post.banner}`
+    const post_path = `../posts/${encodeURIComponent(post.title)}/${encodeURIComponent(post.link)}`
+    card.innerHTML = `
+        <div>
+            <blockquote class="title">
+                <h2>
+                    ${post.title}
+                </h2>
+                <hr>
+            </blockquote>
+            <p>
+                <a href="${post_path}">
+                    <img src="${img_path}">
+                </a>
+            </p>
+            <p>
+                ${post.content.slice(0,515)}...
+            </p>
+            <div style="display: flex; justify-content: space-between;">
+                <a href="${post_path}">
+                    <p style="color: #60a5fa">
+                        Read more
+                    </p>
+                </a>
+                <p>
+                    ${post.date}
+                </p>
+            </div
+        </div>
+    `;
+    return card;
+}
